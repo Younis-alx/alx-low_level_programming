82bd72ebcba1092715f3335e4936521a4065002b
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-16 00:46:07 -0700

    Stopping at preprocessor phase

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ printf("Size of a char: %li byte(s)\n", sizeof(char));
 printf("Size of a int: %li byte(s)\n", sizeof(int));
 printf("Size of a long int: %li byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %li byte(s)\n", sizeof(long long));
-printf("Size of a float: %li byte(s)\n", sizeof(float));
+printf("Size of a float: %li byte(s)", sizeof(float));
 return (0);
 }
