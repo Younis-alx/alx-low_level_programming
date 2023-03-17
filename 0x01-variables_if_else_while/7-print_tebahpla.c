0d75393c4716f693a76e9bd442da6092f108bd3b
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-17 04:54:52 -0700

    README init and first task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Function that will print the alphabet using putchar
+*Return: 0 SUCCES
+*/
+int main(void)
+{
+int i;
+for (i = 122; i >= 97; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
