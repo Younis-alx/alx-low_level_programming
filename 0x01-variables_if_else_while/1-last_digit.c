13a8aeb8f076c0920fd98d86b70fc8a0489d29ee
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-17 04:54:52 -0700

    README init and first task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+int last_digit = n % 10;
+if (last_digit < 6 && last_digit != 0)
+prinf("Last digit of %i is %i and is less than 6 and not 0", n, last_digit);
+else if (Last_digit == 0)
+printf("Last digit of %i is %i and is 0", n, last_digit);
+else
+printf("Last digit of %i is %i and is greater than 5")
+	return (0);
+}
