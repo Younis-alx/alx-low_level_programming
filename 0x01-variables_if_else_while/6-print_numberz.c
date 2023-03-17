3e17ffc98d6a8650e66e4d332cbcf666210c5fac
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-17 04:54:52 -0700

    README init and first task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: 0
+*/
+int main(void)
+{
+int baseNum;
+for (baseNum = 48; baseNum <=57; baseNum++)
+putchar(baseNum);
+putchar('\n');
+return (0);
+}
