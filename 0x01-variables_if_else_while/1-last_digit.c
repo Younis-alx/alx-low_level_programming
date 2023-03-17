c5f9f84aea6e829af8c133ce9c9099e0b7dd24e1
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-17 04:54:52 -0700

    README init and first task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,11 +10,10 @@
 int main(void)
 {
 	int n;
-
+int last_digit = n % 10;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-int last_digit = n % 10;
 if (last_digit < 6 && last_digit != 0)
 printf("Last digit of %i is %i and is less than 6 and not 0", n, last_digit);
 else if (last_digit == 0)
