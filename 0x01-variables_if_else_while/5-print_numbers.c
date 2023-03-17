361ed072db09f37b81db7d5e62e88e3b5ee29e14
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-17 04:54:52 -0700

    README init and first task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - print base 10 numbers
+* Return: 0 SUCCESS
+*/
+int main()
+{
+int base10;
+while (base10 <=9)
+{
+printf("%i", base10);
+++base10;
+}
+printf("\n");
+return(0);
+}
