a45fd18ff334e8016e8c6ba65ff2af05ae52c227
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-17 04:54:52 -0700

    README init and first task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Function that will print the alphabet using putchar
+*Return: 0 SUCCES
+*/
+int main(void)
+{
+for (int i = 97; i < 122; i++)
+i != 122 ? putchar(i) : putchar(i);
+return (0);
+}
