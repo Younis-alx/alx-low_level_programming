74b17498657cf7e3c7e6595f205cd06509d5e478
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-17 04:54:52 -0700

    README init and first task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
     if(n > 0)
-    printf("%i is postive", n);
+    printf("%i is positive", n);
     else if (n == 0)
     printf("%i id zero", n);
     else 
