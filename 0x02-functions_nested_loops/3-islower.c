afc688e9e8714def7c8f47b15011290d58086faf
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-21 04:45:23 -0700

    done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 islower - check if char is lowercase
 Return: 0 if false 1 if ture
 */
-int _islower(int c);
+int _islower(int c)
 {
 int isLower;
 _putchar(c) <= _putchar(122) || _putchar(c) >= _putchar(97) ? isLower = 1 : isLower = 0;
