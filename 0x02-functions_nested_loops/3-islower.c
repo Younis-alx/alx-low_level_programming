bd95b700a8c50b18b662ef3083ceab98489ac462
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-21 04:45:23 -0700

    done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,8 @@
 * @c: char
 *
 * Description: function will use ASCII values to check range
+* the values will use base 10 and get run through logical opterator
+* and (&&) will need both side to be true to output 1
 * Return: 0 if false 1 if ture
 */
 int _islower(int c)
