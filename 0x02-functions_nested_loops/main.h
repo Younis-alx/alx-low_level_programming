0a31c83690fc38e667f3fced58f052d7b1bf67de
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-21 04:45:23 -0700

    done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN
+#define MAIN
 void print_to_98(int n);
 int add(int, int);
 void times_table(void);
@@ -10,3 +12,4 @@ void print_alphabet(void);
 int _isalpha(int c);
 int print_last_digit(int);
 void jack_bauer(void);
+#endif
