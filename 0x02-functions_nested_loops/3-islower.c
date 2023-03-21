10ad32a65ea3f8f03dc6298d65b6c463f30d68cb
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-21 04:45:23 -0700

    done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,6 +6,9 @@ Return: 0 if false 1 if ture
 int _islower(int c)
 {
 int isLower;
-_putchar(c) <= _putchar(122) || _putchar(c) >= _putchar(97) ? isLower = 1 : isLower = 0;
+if (_putchar(c) <= _putchar(122) || _putchar(c) >= _putchar(97))
+isLower = 1;
+else
+isLower = 0;
 return isLower;
 }
