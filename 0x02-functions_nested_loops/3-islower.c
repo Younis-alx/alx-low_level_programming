374fa3af91c356a33ab2969f6340822fc26f23d4
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-21 04:45:23 -0700

    done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
-* islower - check if char is lowercase
-* @c: char
+* _islower - check if char is lowercase
+* @c: first para 
 *
 * Description: function will use ASCII values to check range
-* the values will use base 10 and get run through logical opterator
-* and (&&) will need both side to be true to output 1
 * Return: 0 if false 1 if ture
 */
 int _islower(int c)
