0d305f6877d23a98f881dbf92ee326b815aa75a4
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-21 04:45:23 -0700

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,16 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar(43);
-_putchar('\n');
 return (1);
 }
 else if (n == 0)
 {
 _putchar(43);
-_putchar('\n');
 return (0);
 }
 else
 {
 _putchar(43);
-_putchar('\n');
 return (-1);
 }
 }
