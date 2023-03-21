df413181427b607a76ce1a6e48d93dd822cbf209
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-21 04:45:23 -0700

    done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_last_digit - return last digit
+* @n: num
+*
+* Description: return last num
+* Return: last digits
+*/
+int print_last_digit(int n)
+{
+return (n % 10);
+}
