5deeed8fb0ae1b00c79a568b3f1793f5eb5ccc05
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-21 01:03:58 -0700

    Intro

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point and print _putchar
+ * Return: 0 (SUC)
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Intro too function and nested function
