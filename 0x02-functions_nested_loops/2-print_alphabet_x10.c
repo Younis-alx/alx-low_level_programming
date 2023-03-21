7a1483dbbebfe6fdacca57de6cc1816896f06a10
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-21 04:45:23 -0700

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints alphabets 10x
+* Return: 0
+*/
+void print_alphabet_x10(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 97; j <= 122; j++)
+{
+_putchar(j);
+}
+ _putchar('\n');
+}
+}
