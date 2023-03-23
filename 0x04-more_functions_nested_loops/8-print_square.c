2e6ba404174352ea5b40df6733905d5330137105
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-23 11:22:03 -0700

    isupper alternative

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,7 +7,9 @@
 void print_square(int size)
 {
 if (size <= 0)
+{
 _putchar('\n');
+}
 else
 {
 int i, j;
@@ -15,7 +17,9 @@ int i, j;
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
+{
 _putchar('#');
+}
 _putcahr('\n');
 }
 }
