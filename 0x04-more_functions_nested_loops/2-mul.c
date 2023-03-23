ac414dacfc28df4c60a923c5e952df500b933ea8
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-23 11:22:03 -0700

    isupper alternative

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - multiply two int
+* @a: first int
+* @b: second int
+* Return: product
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN
 #define MAIN
+int mul(int a, int b);
 int _isupper(int c);
 int _isdigit(int c);
 #endif
