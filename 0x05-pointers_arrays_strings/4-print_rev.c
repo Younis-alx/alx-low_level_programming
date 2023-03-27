12998288a2b5883b3b8a5fc726bdf59765b83293
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-27 13:37:31 -0700

    puts alternative

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,11 +4,22 @@
 * @s: string to reverse
 * Return: void
 */
+int _strlen(char *s)
+{
+int count, i;
+count = 0;
+for (i = 0; *(s + i) != '\0'; i++)
+{
+++count;
+}
+return (count);
+}
+
 void print_rev(char *s)
 {
 int len, i;
-len = sizeof(s)/sizeof(char) - 1;
-for (i = len; i > 0; i--)
+len = _strlen(*s);
+for (i = len; i >= 0; i--)
 {
 _putchar(s[i]);
 }
