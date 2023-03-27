5534872f3cf5db4e72bddc1adb1ec00ea5aab5f8
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-27 13:37:31 -0700

    puts alternative

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* rev_string - reverse a string
+* @s: first param
+* Return: void
+*/
+void rev_string(char *s)
+{
+int length, mid, i;
+char temp;
+
+length = 0;
+for (i = 0; *(s + i) != '\0'; i++)
+{
+++length;
+}
+mid = length / 2;
+
+for (i = 0; i < mid; i++)
+{
+temp = s[i];
+s[i] = s[length - i - 1];
+s[length - i -1] = temp;
+}
+
+}
