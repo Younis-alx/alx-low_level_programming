7369dbf2dcdad85fbd7488b57c18c4939774a10f
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-03-29 14:21:48 -0700

    concat two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,20 +8,23 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int flag, i;
-flag = 0;
+int diff, i;
+diff = 0;
 i = 0;
 while (s1[i] != '\0' && s2[i] != '\0')
 {
-if (s1[i] != s2[i])
+if (s1[i] <  s2[i])
 {
-flag = 1;
+diff = s1[i] - s2[i];
 break;
 }
+if (s1[i] >  s2[i])
+{
+diff = s1[i] - s2[i];
+break;
+}
+
 i++;
 }
-if (flag == 0)
-return (0);
-else
-return (1);
+return (diff);
 }
