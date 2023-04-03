de3e1ec2d9d401e95e72bc9b7fa5e9d3dc9cdc95
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-03 11:31:51 -0700

    2D array & pointer to pointer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strchr - returns first occurrence of char @c in string @s else NULL
+* @s: string to parse
+* @c: char to compare
+* Return: pointer or NULL
+*/
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; *(s + i); i++)
+{
+if (*(s + i) == c)
+{
+return (s + i);
+}
+
+}
+
+return (NULL);
+}
