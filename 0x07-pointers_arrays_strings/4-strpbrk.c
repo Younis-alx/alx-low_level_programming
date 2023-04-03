c84694ada69c6db38045e95639c2c2174ba09142
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-03 11:31:51 -0700

    2D array & pointer to pointer

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,7 +10,7 @@ char *_strpbrk(char *s, char *accept)
 int i, j;
 for (i = 0; *(s + i); i++)
 {
-for(j = 0; *(accept + j); j++)
+for (j = 0; *(accept + j); j++)
 {
 if (*(s + i) == *(accept + j))
 {
