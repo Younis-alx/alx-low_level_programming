673904754167e2b96850b3318bd7d7f7e12e2d3b
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-05 12:06:45 -0700

    recursion exercise

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strlen_recursion - find the length of string
+* @s: string
+* Return: length
+*/
+int _strlen_recursion(char *s)
+{
+int i;
+i = 0;
+if (*s)
+{
+++i;
+i += _strlen_recursion(s + 1);
+}
+return i;
+}
