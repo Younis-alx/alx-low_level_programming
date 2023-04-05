7c7f57d26edea1e5333202f6b5fac8fc153b0d5a
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-05 12:06:45 -0700

    recursion exercise

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* factorial - return the factorial of a given number
+* @n: number
+* Return: int
+*/
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+else if (n == 0)
+return (1);
+return (n * factorial(n - 1));
+}
