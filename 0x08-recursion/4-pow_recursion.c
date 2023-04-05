6c39bf1e2562e8fbcfcadfa27b8ece9c7b122c35
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-05 12:06:45 -0700

    recursion exercise

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _pow_recursion - returns the value of @x raised to power to @y
+* @x: first param
+* @y: second param
+* Return: int
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+return (x * _pow_recursion(x, y - 1))
+}
