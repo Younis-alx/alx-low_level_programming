1a44420b7ed22c01bdd790da60975ead4db0f452
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-05 12:06:45 -0700

    recursion exercise

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _print_rev_recursion - prints string in reverse
+* @s: string to reverse
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(++s);
+putchar(*s);
+}
+}
