690fc8ff03ce816488193b861a8b3c18c3799303
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-11 13:09:05 -0700

    dynamic allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,13 +8,14 @@
 */
 char *create_array(unsigned int size, char c)
 {
-char* alloc;
+char *alloc;
 unsigned int i;
 if (size <= 0)
 return (NULL);
 
 alloc = (char *)malloc(size * sizeof(char));
-
+if (alloc == NULL)
+return (NULL);
 for (i = 0; i < size; i++)
 {
 alloc[i] = c;
