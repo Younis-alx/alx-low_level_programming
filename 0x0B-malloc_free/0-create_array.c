52802d7f7d9789635b22908f98283447d677abde
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-11 13:09:05 -0700

    dynamic allocation

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* create_array - creates and array of chars
+* @size: the size of mem alloc
+* @c: chars to fill bytes
+* Return: pointer to array
+*/
+char *create_array(unsigned int size, char c)
+{
+char* alloc;
+unsigned int i;
+if (size <= 0)
+return (NULL);
+
+alloc = (char *)malloc(size * sizeof(char));
+
+for (i = 0; i < size; i++)
+{
+alloc[i] = c;
+}
+return (alloc);
+
+}
