cdcb1b2d4ef0989236df2b146d3f27a0323445a8
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-13 11:38:18 -0700

    function to allocate memory using malloc

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+include "main.h"
+/**
+* array_range - creates an array of int
+* @min: min
+* @max: max
+* Return: int
+*/
+int *array_range(int min, int max)
+{
+int *Pblock;
+int i, j = 0;
+
+if (min > max)
+return (NULL);
+Pblock = malloc(sizeof(*Pblock) * ((max - min) + 1));
+if (Pblock != NULL)
+{
+for (i = min; i <= max; i++)
+{
+Pblock[j] = i;
+j++;
+}
+return (Pblock);
+}
+else
+return (NULL);
+}
