18adf49bcb6a23626507dea528391d0c002a6ef4
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-13 11:38:18 -0700

    function to allocate memory using malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+* string_nconcat - concat two string til n bytes
+* @s1: first string
+* @s2: secong string
+* @n: number of byres to write from s2
+* Return: pointer char to newly allocated mem space
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+int size, i, j;
+char *Pblock;
+
+size = n;
+
+if (!*s1)
+s1 = "";
+else if (!*s2)
+s2 = "";
+
+for (i = 0; *(s1 + i); i++)
+size++;
+
+
+Pblock = malloc((size + 1) * sizeof(char));
+
+if (Pblock == NULL)
+return (NULL);
+
+
+for (i = 0; s1[i]; i++)
+Pblock[i] = s1[i];
+
+
+for (j = 0; j < n; j++)
+Pblock[j + i] = s2[j];
+
+Pblock[j + i + 1] = '\0';
+
+return (Pblock);
+}
