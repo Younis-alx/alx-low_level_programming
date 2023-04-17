bba13871574a3f70a401e560bfd6481f655fa4c7
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-17 11:31:39 -0700

    struct & typdef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+* init_dog - function to initalize variable with type struct dog
+* @d: pointer to struct dog
+* @name: store to name
+* @age: store to age
+* @owner: stgore to owner
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+
+}
