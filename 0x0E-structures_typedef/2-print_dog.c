a05d0d57c93803fe49844ced5acb3543b8a52c1e
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-17 12:14:26 -0700

    struct - print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,21 +7,22 @@
 
 void print_dog(struct dog *d)
 {
-if (d == NULL)
+if (d == NULL )
 return;
 
-if (d->name == NULL)
-printf("Name: (nil)\n");
+if ((*d).name == NULL)
+printf("nil\n");
 else
 printf("Name: %s\n", d->name);
 
-if (d->age <= 0)
-printf("(nil)\n");
+if ((*d).age < 0)
+printf("nil\n");
 else
-printf("Age: %.6f\n", d->age);
+printf("Age: %f\n", d->age);
 
-if (d->owner == NULL)
-printf("(nil)\n");
+if ((*d).owner == NULL)
+printf("nil\n");
 else
 printf("Owner: %s\n", d->owner);
+
 }
