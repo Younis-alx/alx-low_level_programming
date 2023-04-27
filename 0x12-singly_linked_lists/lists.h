0106f356527ca2997103f1ea5b13bebe4a7a898e
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-27 09:40:27 -0700

    linked-list post-local_compile_v2.0.0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST
-#define LIST
+#ifndef LIST_H
+#define LIST_H
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -23,6 +23,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 
 list_t *add_node_end(list_t **head, const char *str);
-
+int _putchar(char c);
 void free_list(list_t *head);
 #endif
