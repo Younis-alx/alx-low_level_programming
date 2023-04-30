b194790ceced3612d653b5231024099a384656ea
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-30 04:00:57 -0700

    linked-list pre-local_compile_v1.0.0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* print_listint - prints all the elements in listint_t
+*@h: head node
+*Return: num of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		printf("%i\n", (*h).n);
+		i++;
+		h = (*h).next;
+	}
+	return (i);
+}
