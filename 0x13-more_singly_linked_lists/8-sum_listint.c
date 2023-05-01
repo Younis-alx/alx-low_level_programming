b9fb5f6b73c0fa253d0b7031cf7b936ea9942d40
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-05-01 04:44:43 -0700

    sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* sum_listint - sum of all data (n) of a listint
+* @head: struct
+* Return: int
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	while (head)
+	{
+	sum += head->n;
+	head = head->next;
+	}
+	return (sum);
+}
