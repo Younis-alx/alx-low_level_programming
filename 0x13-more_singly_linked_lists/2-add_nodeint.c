ebd2fd9abecaeb2fd1346ed9f820ffc5207c4b53
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-30 22:50:21 -0700

    reformatted the comment as per betty and add edge case

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,19 +1,23 @@
 #include "lists.h"
-/*
-*function that adds a new node at the beginning of a listint_t list.
+/**
+* add_nodeint - function that adds a new node
 *@head: head ptr
 *@n: number inside the node
 *Return: the address of the new element, or NULL if it failed
-
+*
 */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-  listint_t *new;
-  new = malloc(sizeof(listint_t));
-  new->n = n;
-  new->next = *head;
-  *head = new;
+	listint_t *new;
 
-  return (new);
+	new = malloc(sizeof(listint_t));
+
+	if (!new)
+	return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
