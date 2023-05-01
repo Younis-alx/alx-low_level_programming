4d08c582988e142bb1fcb32f5e4a2a6841b0f0d2
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-05-01 07:26:20 -0700

    betty fixes

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *del_node;
 	unsigned int i = 0;
 
-	if (!*head || index > list_len(*head))
+	if (!*head || index > len(*head))
 		return (-1);
 
 	tmp = *head;
