8e6d0b80f094d60340f710bca6fa875fad743a5e
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-04-30 23:58:27 -0700

    node at the end of listint_t

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+* add_nodeint_end - function that adds new node at the end of a list
+*@head: ptr to a struc
+*@n: value to store on struc
+*Return: int
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *tail_node = *head;
+
+	new = malloc(sizeof(listint_t));
+
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+	*head = new;
+	return (new);
+
+	}
+
+	while (tail_node->next)
+		tail_node = tail_noce->next;
+
+	tail_node->next = new;
+
+	return (new);
+}
