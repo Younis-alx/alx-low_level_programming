75cacf87ffe8c381426750c72d2ca748ef55a415
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-05-04 04:16:07 -0700

    convert bin to u-int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* binary_to_uint - converts a binary number to uint
+* @b: bin num
+*Return: uint
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	int max_idx = 0, multi = 1;
+	unsigned int res = 0;
+
+	if(!b)
+		return (0);
+
+	max_idx = len(b) - 1;
+
+	while (max_idx >= 0)
+	{
+		if (b[max_idx] != '0' && b[max_idx] != '1')
+			return (0);
+
+		res += multi * (b[max_idx] - '0');
+		multi <<= 1;
+		max_idx--;
+	}
+	return (res);
+}
